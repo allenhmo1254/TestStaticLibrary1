f4c0a6843414b3dc5c0193a5d068a3f430fa3508
zhongjie3 <zhongjie3@staff.weibo.com>
2018-01-15 16:52:42 +0800

    提交代码

---
+++ TestStaticLibrary1/TestStaticLibrary1/TestStaticLibrary1.h
@@ -0,0 +1,13 @@
+//
+//  TestStaticLibrary1.h
+//  TestStaticLibrary1
+//
+//  Created by zhongjie3 on 2018/1/15.
+//  Copyright © 2018年 zhongjie3. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestStaticLibrary1 : NSObject
+
+@end
---
+++ TestStaticLibrary1/TestStaticLibrary1/TestStaticLibrary1.m
@@ -0,0 +1,13 @@
+//
+//  TestStaticLibrary1.m
+//  TestStaticLibrary1
+//
+//  Created by zhongjie3 on 2018/1/15.
+//  Copyright © 2018年 zhongjie3. All rights reserved.
+//
+
+#import "TestStaticLibrary1.h"
+
+@implementation TestStaticLibrary1
+
+@end
